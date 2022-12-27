42e8dc8eccbac76109eda1948d80373847e04e37
L-IZ1 <111218789+L-IZ1@users.noreply.github.com>
2022-12-27 18:08:23 +0100

    created function updateTime

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-alet("hello");
+function updateTime(){
+let selectFreiburg=document.querySelector("#freiburg");
+let selectFreiburgDate=selectFreiburg.querySelector(".date")
+let selectFreiburgTime=selectFreiburg.querySelector(".time")
+selectFreiburgDate.innerHTML=moment().format("dddd, MMMM Do YYYY");
+selectFreiburgTime.innerHTML=moment().tz("Europe/Berlin").format("h:mm:ss A");
+
+}
+updateTime();
+setInterval(updateTime,1000);
+
+
+    //   function displayDateTime(event) {
+    //     let Time = moment()
+    //       .tz(event.target.value)
+    //       .format("h:mm A");
+    //     let Date = moment()
+    //       .tz(event.target.value)
+    //       .format("dddd, MMMM Do YYYY");
+
+
+    //     if (event.target.value >0) {
+    //       {Time} ;
+    //     }
+    //   }
+
+    //   let selectCity = document.querySelector("#cities");
+    //   selectCity.addEventListener("change", displayDateTime);
